758de654ea4fb426984fc994ddee43ca72cebb42
ColdCat-ship-it <michael_0409@163.com>
2026-09-16 22:38:34 +0000

    override induction

--- Induction.v
+++ Induction.v
@@ -579,11 +579,26 @@ Proof.
 Theorem expand_para : forall m n: nat,
   m * S n = m + m * n.
 Proof. 
+Proof.
+intros m n.
+induction m as [| m H].
+-reflexivity.
+-simpl.
+rewrite H.
+rewrite add_shuffle3. reflexivity.
+Qed.
   
+<<<<<<< HEAD
   intros m n.
   induction m as [| m H].
   - reflexivity.
   - simpl. rewrite H. rewrite add_shuffle3. reflexivity. Qed.
+=======
+(* intros n m.
+  induction m as [| m H].
+  - rewrite mul_0_r. rewrite mult_n_1. rewrite add_0_r. reflexivity.
+  - rewrite -> H.  rewrite <- H.  *)
+>>>>>>> cda01e332dd59a410543ca7f467f98acb6b0467a
 
   (* intros n m.
   replace (S n) with (n + 1).
@@ -721,9 +736,15 @@ Proof.
   (* FILL IN HERE *) intros b.
   induction b as [].
   - reflexivity.
+<<<<<<< HEAD
   - simpl. rewrite add_comm. reflexivity.
   - simpl. rewrite IHb. simpl. rewrite add_comm. reflexivity.
   Qed.
+=======
+  - replace (B0 b) with b. rewrite <- IHb. reflexivity. 
+  - reflexivity.
+  Admitted.
+>>>>>>> cda01e332dd59a410543ca7f467f98acb6b0467a
 
 
 (** [] *)
